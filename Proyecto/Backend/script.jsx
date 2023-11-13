e92b782b16a8f3df436bc42d0d608254cbb98bc6
Federico Salinas <fede_ms@outlook.es>
2023-11-13 11:29:47 -0300

    Update

--- Proyecto/Backend/script.jsx
+++ Proyecto/Backend/script.jsx
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('header');
-  
-    function handleScroll() {
+  const header = document.querySelector('header');
+
+  function handleScroll() {
       const scrollPosition = window.scrollY;
-      if (scrollPosition === 0) {
+
+      // Verifica si la posición de desplazamiento es mayor que 0.
+      if (scrollPosition <= 0) {
+        // Estás en la parte superior de la página
         header.classList.add('fondo-inicial');
         header.classList.remove('fondo-scroll');
       } else {
+        // Estás desplazándote hacia abajo
         header.classList.remove('fondo-inicial');
         header.classList.add('fondo-scroll');
       }
-    }
-  
-    window.addEventListener('scroll', handleScroll);
-  });
-  
+      
+  }
+
+  window.addEventListener('scroll', handleScroll);
+});
